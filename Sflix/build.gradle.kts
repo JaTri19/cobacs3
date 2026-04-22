d7b6105e01c423a53a6b0c2d6a06c1197dc056af
JaTri19 <jarwo.tri28@gmail.com>
2026-04-23 02:24:28 +0700

    Remove isCrossPlatform to fix build

--- Sflix/build.gradle.kts
+++ Sflix/build.gradle.kts
@@ -4,7 +4,6 @@ cloudstream {
     language = "id"
     description = "Sflix - Movie & Series subtitle indonesia"
     authors = listOf("Asm0d3usX")
-	isCrossPlatform = true
     status = 1
     tvTypes = listOf(
         "TvSeries",
